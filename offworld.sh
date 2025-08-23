85de0b493662adc47d97fd8125fbf18087e51755
Kjetil Midtlie <kjetil.midtlie@webstep.no>
2025-08-23 16:30:57 +0200

    offworld helper script

---
+++ offworld.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# curl only tgz from github or CACHE
+GH_PRE=${GH_PRE:-https://github.com}
+
+# mac-hacks
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  alias tar=gtar
+  alias awk=gawk
+fi
+
+curl_untar(){
+  mkdir -p $2
+  curl -sL "$1" |\
+    gtar -xvzf - --strip-component=1 -C "$2"
+}
+
+# gh_cache_fetch <github_repo@ref> <dest-dir>
+gh_cache_fetch() {
+  mkdir -p $2
+  if echo $1|grep -q "@";then
+    repo=${1%@*}
+    ref=${1#*@}
+    #if [ ref = "main" ] || [ $ref = "master" ];then
+    curl_untar $GH_PRE/$repo/archive/$ref.tar.gz $2
+  else
+    echo "No REF given in arg1 <github_rep@REF>"
+  fi
+}
+
