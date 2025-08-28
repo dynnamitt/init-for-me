a8ba741c74c2f796846756e759f41d61396f1c48
Kjetil Midtlie <kjetil.midtlie@webstep.no>
2025-08-28 22:30:00 +0200

    an update

--- relink.mk
+++ relink.mk
@@ -8,24 +8,27 @@ conf_real_path := ~/.config/$(NVIM_APPNAME)
 
 .PHONY: clean_dot_git clean relink
 
+relink: $(share_cache)/lazy/LazyVim/init.lua 
+	ln -sfv $$PWD/$(repo_cache) $(conf_real_path)
+	ln -sfv $$PWD/$(share_cache)/lazy $(share_real_path)
+	# kitty
+	mkdir -p ~/.config/kitty
+	ln -sfv $$PWD/kitty.conf ~/.config/kitty
+	ln -sfv $$PWD/kitty-themes ~/.config/kitty
+
 $(share_cache)/lazy/LazyVim/init.lua: $(repo_cache)/init.lua
 	@echo "FIRST run ´NVIM_APPNAME=$(NVIM_APPNAME) nvim´ .. and come back to this shell"
 	read # allow user exit here
 	mkdir -pv $$PWD/$(share_cache)
 	# the flip !
 	mv -v $(share_real_path)/lazy $(share_cache)
-	ln -s $$PWD/$(share_cache)/lazy $(share_real_path)
-	# call self
+	# call self 
 	$(MAKE) -f $(lastword $(MAKEFILE_LIST)) NVIM_APPNAME=$(NVIM_APPNAME) clean_dot_git
 
 $(repo_cache)/init.lua:
 	git clone https://github.com/LazyVim/starter $(repo_cache)
 	rm -rf $(repo_cache)/.git
-	ln -s $$PWD/$(repo_cache) $(conf_real_path)
-
-relink:
 	ln -sv $$PWD/$(repo_cache) $(conf_real_path)
-	ln -sv $$PWD/$(share_cache)/lazy $(share_real_path)
 
 clean_dot_git:
 	find $(share_cache) -name ".git" -type d -exec echo "Can delete: {}" \;
